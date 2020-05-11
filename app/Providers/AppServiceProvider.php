5bc0a7189ae59b9ecea3785c4096f17baf3bf223
David Oliver <davooliver1964@gmail.com>
2020-05-11 08:27:50 -0400

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+// use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Passport::routes();
+    }
+}
---
+++ database/factories/ParagraphFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Paragraph;
+use Faker\Generator as Faker;
+
+$factory->define(Paragraph::class, function (Faker $faker) {
+    return [
+        'post-id' => $faker->num,
+        'body' => $faker->paragraph
+    ];
+});
